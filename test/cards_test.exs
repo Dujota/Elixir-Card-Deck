9d1d61253529751d9e1c33974e4962a2febbf250
Denis <ddujota@internationalsafety.com>
2021-02-19 13:43:24 -0500

    updated test docs

--- test/cards_test.exs
+++ test/cards_test.exs
@@ -1,9 +1,15 @@
 defmodule CardsTest do
+  @doc """
+    There 3 distinct type of tests:
+    1.) doctest <- tests code examples in doc tags
+       - it will look for the patterns of ## Examples
+       and then it will run any block of code it sees as the triple tab indentation
+       - it will make an assersion  of whatever the iex last line is will equal the value specified in the doc
+         IE: iex> hand
+         ["Ace of Spades"]
+         it will assert hand == ["Ace of Spades"]
+  """
+
   use ExUnit.Case
   doctest Cards
-
-  test "greets the world" do
-    # assert Cards.hello() == :world
-    assert 1 + 1 == 3
-  end
 end
